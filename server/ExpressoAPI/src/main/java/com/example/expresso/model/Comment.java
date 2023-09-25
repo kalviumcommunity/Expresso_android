deb43611197cc7ca7e15b740033cdddbca182a33
Ayush Shrivastava <112609589+Ayush604141@users.noreply.github.com>
2023-09-25 20:58:24 +0530

    Define Classes and Initialize Spring Application.
    I have defined the User, Comments, Blogs and necessary classes with the required data members.
    I have also initialized the spring app for RESTful API.

---
+++ server/ExpressoAPI/src/main/java/com/example/expresso/model/Comment.java
@@ -0,0 +1,10 @@
+package com.example.expresso.model;
+
+public class Comment {
+    private long commentId;
+    private long blogId;
+    private long userId;
+    private String comment;
+    private int commentLikes;
+    private long commentCreatedAt;
+}
---
+++ server/ExpressoAPI/src/main/java/com/example/expresso/model/User.java
@@ -0,0 +1,9 @@
+package com.example.expresso.model;
+
+public class User {
+    private long userId;
+    private String username;
+    private String userEmail;
+    private String userPassword;
+    private long userCreatedAt;
+}
